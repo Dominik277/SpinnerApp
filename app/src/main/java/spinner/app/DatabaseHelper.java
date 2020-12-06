fb37dec6c26927443efa4fa288286e7d2c221eda
Dominik277 <dominikdoric277@gmail.com>
2020-12-06 22:44:49 +0100

    Implementirana onCreate metoda i dodano objasnjenje za svaku liniju koda.

--- app/src/main/java/spinner/app/DatabaseHelper.java
+++ app/src/main/java/spinner/app/DatabaseHelper.java
@@ -26,8 +26,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_NAME = "name";
 
 
-    //
-    public DatabaseHelper(@Nullable Context context) {
+    //kada zelimo stvoriti neku podklasu prvo moramo stvoriti instancu nadklase
+    //da bi to napravili,prva linija unutar konstruktora mora zvati konstruktor
+    //nadklase.To radimo preko kljucne rijeci super()
+    //context -->referenca na kontekst,kako bi taj kontekst mogao biti prosljeđen roditelju
+    //DATABASE_NAME -->konstanta klase,ime baze podataka(String)
+    //DATABASE_VERSION -->konstanta klase,verzija baze podataka(int)
+    private DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSTION);
     }
 
@@ -42,6 +47,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        //ovdje smo stvorili CREATE_ITEM_TABLE  varijablu u koji ce biti tipa string,u nju ce biti
+        //pohranjeno sve s desne strane.Na desnoj strani imamo naredbu CREATE TABLE koja je zaduzena
+        //za kreiranje tablice, na nju nadodamo konstatnu TABLE_NAME u kojoj je pohranjeno ime koje
+        //zelimo za tablicu, nakon toga nam dolazi zagrada, a u zagradi se navodi sve sto zelimo da
+        //bude u tablici,znaci u tablici ce biti COLUMN_ID koji u sebi ima ime koje ce predstavljati
+        //njegov redak,on biti tipa int i i bit ce jedinstven,nakon toga imamo redak koji ce
+        //predstavljati ime a iskazan je kao COLUMN_NAME u koji je spremljen tekst "name"
+        //sve te naredbe su spremljene u varijablu CREATE_ITEM_TABLE i te naredbe sada
+        //referenciramo preko toga imena
+        String CREATE_ITEM_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_NAME + " TEXT)";
+
+        //ova metoda izvrsava samo jednu SQL operaciju koja NIJE SELECT ili bilo koja druga
+        //SQL operacija koja vraca neku vrijednost jer ova metoda ne vraca vrijednost
+        //mi smo samo u varijablu CREATE_ITEM_TABLE spremili naredbu,ali ju mi nismo jos
+        //izvrsili, a izvrsiti ju mozemo jedino preko metode execSQL() kojoj kao parameter
+        //navodimo tu varijablu CREATE_ITEM_TABLE koja u sebi sadrzi sve potrebno za stvaranje
+        //nove tablice sa potrebnim podacima o redcima i imenima redaka
+        db.execSQL(CREATE_ITEM_TABLE);
+
     }
 
     //ova metoda je pozvana kada baza podataka mora biti azurirana,
@@ -55,6 +80,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //primjer: imamo aplikaciju na google play-u i zelimo ju azurirati a to zahtjeva i azuriranje
     //baze podataka, kada ju azuriramo u aplikacijama korisnika koji vec koriste staru verziju ce
     //biti pozvana onUpgrade metoda i baza podataka ce se azurirati
+    //SQliteDatabase db -->
+    //oldVersion -->
+    //newVersion -->
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
--- app/src/main/java/spinner/app/MainActivity.java
+++ app/src/main/java/spinner/app/MainActivity.java
@@ -1,14 +1,32 @@
 package spinner.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    //ovdje smo samo deklarirali varijable i njihove tipove podataka
+    //nismo jos nista u te varijable spremili,prazne su
+    Spinner spinner;
+    Button btnAdd;
+    EditText inputLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //nakon sto smo gore odredili imena varijabli sada u njih spremamo
+        //ono sto je s desne strane.S desne strane se nalazi metoda findViewById()
+        //koja odlazi u XML datoteku i tamo nalazi atribute koji se nazivaju
+        //u nasem slucaju spinner,btnAdd i inputLabel,te te atribute sprema u
+        //odgovarajuce varijable onako kako smo naveli u kodu
+        spinner = findViewById(R.id.spinner);
+        btnAdd = findViewById(R.id.btnAdd);
+        inputLabel = findViewById(R.id.inputLabel);
+
     }
 }
