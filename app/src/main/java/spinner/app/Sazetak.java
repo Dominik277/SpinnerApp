52e2c07aba5f799afb57533ba16128020875f1e6
Dominik277 <dominikdoric277@gmail.com>
2020-12-08 19:16:09 +0100

    Dodano objasnjenje za DatabaseHelper klasu.

--- app/src/main/java/spinner/app/Sazetak.java
+++ app/src/main/java/spinner/app/Sazetak.java
@@ -19,7 +19,34 @@ MainActivity klasa
 DatabaseHelper klasa
 ---------------------
 -DatabaseHelper klasu mozemo zamislit kao klasu u kojoj se odvijaju sve operacije unosenja podataka u bazu.
- Na pocetku klase smo definirali neke konstante preko kojih cemo referencirati određene SQL naredbe.
+ Na pocetku klase smo definirali neke konstante preko kojih cemo referencirati određene SQL naredbe.Npr u slucaju
+ kada zelimo imenovati kako ce se nasa tablica zvati necemo napisati "labels" nego DATABASE_NAME jer smo u tu
+ konstantu spremili string "label".Isto tako smo i za ostale dijelove tablice kao sto su redci i stupci.
+ Kada napravimo neku podklasu prvo moramo stvoriti instancu nadklase, a to radimo tako da unutar konstruktora
+ pozovemo konstruktor nadklase, a to radimo pomocu kljucne rijeci super() kojoj kao parametre predajemo context,
+ ime baze podataka i verziju baze podataka.Ovo sve nam je bilo potrebno za instaciranje objekta ove klase.Nakon toga
+ smo dosli do implemenitranih metoda koje smo morali implementirati jer je DatabaseHelper klasa podklasa klase
+ SQLiteOpenHelper koja je abstraktna sto znaci da moramo implementirati sve metode koje se u njoj nalaze.Prva od
+ metoda je onCreate(), ova metoda se poziva kada se baza podataka prvi puta pravi i unutar ove metode moraju biti sve
+ narebe sto se tice unosenja,brisanja,azuriranja podataka u bazi.Nakon toga dolazi onUpgrade() metoda koja se poziva
+ kada podaci unutar baze moraju biti azurirani, odnosno kada se verzija baze podataka mjenja.Kada otvorimo bazu i
+ vidimo da u njoj nisu neki podaci koji zelimo da budu, ne moramo dropati bazu i ispocetka ju praviti nego ju samo
+ azuriramo, a to azuriranje bi se trebalo izvrsiti u tijelu ove metode.Poslije toga dolazi metoda insertLabel koja
+ nam sluzi za unosenje onoga sto unesemo u EditText u bazu podataka.Prvo smo stvorili objekt od klase
+ SQLiteDatabase db koji nam zapravo predstavlja bazu podataka te preko metode getWritaableDatabase omogucili da se
+ u tu bazu mogu unositi podaci, ali i iscitavati iz nje.Nakon toga smo stvorili objekt values tipa ContentValue koji
+ u sebe prima podatke u obliku key/value parova s pomocu metode put.Key/value parovi zamisljamo tako da nam key predstavlja
+ neki string, neki opis ili ime neke vrijednosti, a value je ta vrijednost koju key opisuje.Nakon toga na objektu db
+ koji kao sto sam vec rekao predstavlja bazu podataka dodajemo metodu insert koja je mozemo to tako reci najbitnija, sve
+ dok nismo pozvali metodu insert zapravo jos nista nije bilo spremljeno u bazu, do ovog trenutka smo mi samo pripremali
+ sto ce biti ubaceno, a insert metoda to ubacuje u bazu.Kao parametre joj predajemo ime baze podataka i podatke koje treba
+ spremiti te naposljetku na db objekt dodaje close() metodu koja prekida konekciju objekt s bazom podataka.Na kraju ove klase
+ napravili smo metodu getAllLabels() koja je zaduzena za povlacenje svih podataka iz baze i slaganja u List-u.Prva naredba u
+ ovoj klasi nam samo govori da smo stvorili objekt list u kojem ce biti svi podaci koje povucemo iz baze.U drugoj naredbi smo
+ stvorili varijablu selectQuery tipa String koja jednostavno u sebi sadrzava query naredbe, nista drugo.Nakon toga smo s
+ getReadableDatabase rekli da cemo iscitavati podatke iz baze, a da ih necemo upisivati.Te smo naposljetku napravili Cursor
+ objekt pomocu kojeg smo navigirali koje tocno podatke zelimo povuci iz baze i obavezno zatvorili konekciju s bazom i cursorom
+ pomocu metode close().
 
 
 
