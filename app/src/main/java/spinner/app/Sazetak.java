33316532ada8a72bfbf7b52c499f364991d5a0e3
Dominik277 <dominikdoric277@gmail.com>
2020-12-08 18:47:23 +0100

    Dodano objasnjenje projekta.

--- app/src/main/java/spinner/app/Sazetak.java
+++ app/src/main/java/spinner/app/Sazetak.java
@@ -1,4 +1,14 @@
 /*
+-----------
+O PROJEKTU
+-----------
+-Ovaj mali projekt nam prikazuje uporabu SQLite baze podataka, te prikazivanja podataka koji su pohranjeni u bazu
+ s pomoću Spinner-a te s pomoću padajućeg menija.Na vrhu activitia imamo EditText view u koji unosimo zeljeni tekst
+ koji zelimo pohraniti u bazu, klikom na gumb Dodaj taj podatak se sprema u bazu.Nakon toga u Spinneru s pomocu
+ padajuceg menija mozemo prikazati sve podatke koje smo unijeli u bazu podataka.Također imamo i kontrolu da ako
+ korisnik kojim slucajem nije unio nista u EditText view onda se na dnu ekrana u Toast poruci govori korisniku da
+ nije unio nista u bazu podataka te da ponovi postupak.
+
 
 -------------------
 MainActivity klasa
@@ -8,7 +18,8 @@ MainActivity klasa
 ---------------------
 DatabaseHelper klasa
 ---------------------
-
+-DatabaseHelper klasu mozemo zamislit kao klasu u kojoj se odvijaju sve operacije unosenja podataka u bazu.
+ Na pocetku klase smo definirali neke konstante preko kojih cemo referencirati određene SQL naredbe.
 
 
 
